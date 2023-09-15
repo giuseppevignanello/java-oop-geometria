095feaa28efa7cc0c888f7cd0e07c1a66115881b
giuseppevignanello <peppe.vignanello@gmail.com>
2023-09-15 15:15:28 +0200

    add main class and create three rettangles

---
+++ src/org/lessons/java/geometria/Main.java
@@ -0,0 +1,30 @@
+package org.lessons.java.geometria;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		Rettangolo[] rettangles = new Rettangolo[3]; 
+		
+		System.out.println("Insert base and height for three rettangles");
+		
+		for (int i = 0; i < 3; i++) {
+			System.out.println("Base rettangle " + (i + 1) + ": ");
+			int base = sc.nextInt();
+			System.out.println("Height rettangle " + (i + 1) + ": ");
+			int heigth = sc.nextInt();
+			
+			rettangles[i] = new Rettangolo(base, heigth);
+			
+		}
+		
+		for (int i = 0; i < rettangles.length; i++) {
+			System.out.println("Rettangle " + (i+1) + " base: " + rettangles[i].base);
+			System.out.println("Rettangle " + (i+1) + " height: " + rettangles[i].height);
+			System.out.println("Rettangle " + (i+1) + " perimeter: " + rettangles[i].calcolaPerimetro());
+			System.out.println("Rettangle " + (i+1) + " area: " + rettangles[i].calcolaArea());
+		} 
+	}
+}
