959201e491f27f6b2bd60feabeb53e801b006a70
giuseppevignanello <peppe.vignanello@gmail.com>
2023-09-15 15:35:01 +0200

    fix typos

--- src/org/lessons/java/geometria/Main.java
+++ src/org/lessons/java/geometria/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		Rettangolo[] rettangles = new Rettangolo[3]; 
+		Rettangolo[] rectangles = new Rettangolo[3]; 
 		
 		System.out.println("Insert base and height for three rettangles");
 		
@@ -16,17 +16,17 @@ public class Main {
 			System.out.println("Height rettangle " + (i + 1) + ": ");
 			int heigth = sc.nextInt();
 			
-			rettangles[i] = new Rettangolo(base, heigth);
+			rectangles[i] = new Rettangolo(base, heigth);
 			
 		}
 		
-		for (int i = 0; i < rettangles.length; i++) {
-			System.out.println("Rettangle " + (i+1) + " base: " + rettangles[i].base);
-			System.out.println("Rettangle " + (i+1) + " height: " + rettangles[i].height);
-			System.out.println("Rettangle " + (i+1) + " perimeter: " + rettangles[i].calcolaPerimetro());
-			System.out.println("Rettangle " + (i+1) + " area: " + rettangles[i].calcolaArea());
+		for (int i = 0; i < rectangles.length; i++) {
+			System.out.println("Rectangle " + (i+1) + " base: " + rectangles[i].base);
+			System.out.println("Rectangle " + (i+1) + " height: " + rectangles[i].height);
+			System.out.println("Rectangle " + (i+1) + " perimeter: " + rectangles[i].calcolaPerimetro());
+			System.out.println("Rectangle " + (i+1) + " area: " + rectangles[i].calcolaArea());
 			System.out.println();
-			rettangles[i].draw();
+			rectangles[i].draw();
 			System.out.println();
 		} 
 	}
